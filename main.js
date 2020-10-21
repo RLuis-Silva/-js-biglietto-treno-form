4c02d89346eabfab471c71b71b83a525efa5b73d
Luis Silva <s.ros.luis@gmail.com>
2020-10-21 15:53:28 +0200

    inizio add js

---
+++ main.js
@@ -0,0 +1,10 @@
+// console.log("funziona JS");
+var nomeCognomePasseggero = document.getElementById('nomeCognome');
+var kmPasseggero = document.getElementById('km');
+var etaPasseggero = document.getElementById('eta');
+
+var clickGenera = document.getElementById('btnGenera');
+
+clickGenera.addEventListener("click", genera(){
+    
+});
