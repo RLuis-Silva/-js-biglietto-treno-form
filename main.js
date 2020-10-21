dcf758d72a341138b5270bda91deeb4ee095a6ee
Luis Silva <s.ros.luis@gmail.com>
2020-10-21 17:02:58 +0200

    add calcolo prezzo biglietto con relativo sconto

--- main.js
+++ main.js
@@ -5,10 +5,39 @@ var bottoneGenera = document.getElementById('btnGenera');
 var clickAnnulla = document.getElementById('btnAnnulla');
 
 bottoneGenera.addEventListener('click', function(){
-    // console.log('funziona sta merda di click!');
+    // console.log('funziona sta merda di click!'); //msg di debug
 
-    //ottiene dati del passeggero
+    //ottiene dati del passeggero dagli input utilizzando value
     var nomeCognomePasseggero = document.getElementById('nomeCognome').value;
-    var kmPasseggero = document.getElementById('km').value;
+    console.log(nomeCognomePasseggero);
+
+    var kmPercorso = document.getElementById('km').value;
+    console.log(kmPercorso + " Km");
+
     var etaPasseggero = document.getElementById('eta').value;
+    console.log(etaPasseggero);
+
+
+    //calcolo biglietto con sconto
+    var prezzoKm = 0.21;
+    var prezzoTotale = 0.21 * kmPercorso;
+    var sconto20 = prezzoTotale / 100 * 20;
+    var sconto40 = prezzoTotale / 100 * 40;
+    var prezzoBiglietto;
+    var tipoOfferta = "Biglietto Standard";
+    
+    if(etaPasseggero == minorenne){
+        //sconto di 20%
+        prezzoBiglietto = prezzoTotale - sconto20;
+        tipoOfferta = "Sconto Minorenni";
+    }else if(etaPasseggero == over65){
+        prezzoBiglietto = prezzoTotale - sconto40;
+        tipoOfferta = "Sconto Over 65";
+    }else{
+        prezzoBiglietto;
+    }
+
+    //output decimali e valuta
+    prezzoBiglietto = prezzoBiglietto.toFixed(2) + " euro";
+
 });
