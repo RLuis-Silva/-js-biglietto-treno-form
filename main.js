1f20589849b92490b0420f0f591f1de485a82937
Luis Silva <s.ros.luis@gmail.com>
2020-10-21 16:42:08 +0200

    add  var per ottenere dati passeggero dagli input

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 console.log("funziona JS");
-// var nomeCognomePasseggero = document.getElementById('nomeCognome');
-// var kmPasseggero = document.getElementById('km');
-// var etaPasseggero = document.getElementById('eta');
 
-var bigl = document.getElementById('biglietto');
+// var bigl = document.getElementById('biglietto');
 var bottoneGenera = document.getElementById('btnGenera');
 var clickAnnulla = document.getElementById('btnAnnulla');
 
 bottoneGenera.addEventListener('click', function(){
-    console.log('funziona sta merda di click!');
+    // console.log('funziona sta merda di click!');
+
+    //ottiene dati del passeggero
+    var nomeCognomePasseggero = document.getElementById('nomeCognome').value;
+    var kmPasseggero = document.getElementById('km').value;
+    var etaPasseggero = document.getElementById('eta').value;
 });
